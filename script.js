ddb843012463de701239d21ab720f16a691d4394
Anne Charlene S. Baldemor <baldemor2020177@mkt.ceu.edu.ph>
2024-08-22 15:24:13 +0800

    final

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
-
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
-window.onscroll=() => {
+window.onscroll = () => {
     sections.forEach(sec => {
         let top = window.scrollY;
-        let offset = sec.offsetTop -150;
+        let offset = sec.offsetTop - 150; // Adjust as needed
         let height = sec.offsetHeight;
         let id = sec.getAttribute('id');
 
-        if(top >= offset && top < offset + height){
-            navLinks.forEach(links => {
-                links.classList.remove('active');
-                document.querySelector('header nav a [href'=' + id + ]').classList.add('active');
-        
-            })
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(link => {
+                link.classList.remove('active');
+                if (link.getAttribute('href') === '#' + id) {
+                    link.classList.add('active');
+                }
+            });
         }
-    })
-}
+    });
+};
 
 menuIcon.onclick = () => {
-    menuIcon.classList.toggle('bx-x');
-    navbar.classList.toggle('active');
-}
+    menuIcon.classList.toggle('bx-x'); // For changing the menu icon
+    navbar.classList.toggle('active'); // To show/hide the navbar
+};
